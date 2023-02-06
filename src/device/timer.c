ead9f42980293e75859fa53ac36b962e604fd1ed
litrehinn <litrehinn@smail.nju.edu.cn>
2023-02-06 23:29:10 +0800

    add device

--- src/device/timer.c
+++ src/device/timer.c
@@ -12,21 +12,17 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   }
 }
 
-#ifndef CONFIG_TARGET_AM
 static void timer_intr() {
   if (nemu_state.state == NEMU_RUNNING) {
     extern void dev_raise_intr();
     dev_raise_intr();
   }
 }
-#endif
 
 void init_timer() {
   rtc_port_base = (uint32_t *)new_space(8);
-#ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("rtc", CONFIG_RTC_PORT, rtc_port_base, 8, rtc_io_handler);
-#else
+  
   add_mmio_map("rtc", CONFIG_RTC_MMIO, rtc_port_base, 8, rtc_io_handler);
-#endif
+
   IFNDEF(CONFIG_TARGET_AM, add_alarm_handle(timer_intr));
 }
